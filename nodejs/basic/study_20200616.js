26b276501ef3cc27758b07ddc23454f3ff43ddab
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-06-16 23:16:53 +0900

    20200616 学習内容を追加

---
+++ nodejs/basic/study_20200616.js
@@ -0,0 +1,53 @@
+// オブジェクトモデルの詳細
+// クラスベース言語とプロトタイプベース言語
+// クラスの定義
+// サブクラスと継承
+// プロパティの追加と削除
+// 相違点の概要
+
+// 事例 : 従業員モデル
+
+// 階層の作成
+function Employee() {
+    this.name='';
+    this.dept='general';
+}
+function Manager() {
+    Employee.call(this);
+    this.reports = [];
+}
+Manager.prototype = Object.create(Employee.prototype);
+Manager.prototype.constructor = Manager;
+
+function WorkerBee() {
+    Employee.call(this);
+    this.projects = [];
+}
+WorkerBee.prototype = Object.create(Employee.prototype);
+WorkerBee.prototype.constructor = WorkerBee;
+
+function SalesPerson() {
+    WorkerBee.call(this);
+    this.dept = 'sales';
+    this.quota =100;
+}
+SalesPerson.prototype = Object.create(WorkerBee.prototype);
+SalesPerson.prototype.constructor = SalesPerson;
+
+function Engineer() {
+    WorkerBee.call(this);
+    this.dept = 'engineering'
+    this.machine = '';
+}
+Engineer.prototype = Object.create(WorkerBee.prototype);
+Engineer.prototype.constructor = Engineer;
+
+// 簡単な定義によるオブジェクトの作成
+var jim = new Employee;         // 括弧は省略可能
+var sally = new Manager;
+var mark = new WorkerBee;
+var fred = new SalesPerson
+var jane = new Engineer;
+
+// オブジェクトのプロパティ
+// プロパティの継承
