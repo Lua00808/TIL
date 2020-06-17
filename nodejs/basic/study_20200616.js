72fdd93a8f75bced12b19877600ac889b3b75a81
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-06-17 22:55:27 +0900

    20200617 学習内容を追加

--- nodejs/basic/study_20200616.js
+++ nodejs/basic/study_20200616.js
@@ -51,3 +51,45 @@ var jane = new Engineer;
 
 // オブジェクトのプロパティ
 // プロパティの継承
+
+// プロパティの追加
+
+// より柔軟なコンストラクター
+function Employee(name, dept) {
+    this.name = name || '';
+    this.dept = dept || 'general';
+}
+
+function WorkerBee(projs) {
+    this.name = '';
+    this.projects = projs || [];
+}
+WorkerBee.prototype = new Employee;
+
+function Engineer(mach) {
+    this.dept = 'engoneering';
+    this.machine = mach || '';
+}
+Engineer.prototype = new WorkerBee;
+
+// その2
+function Engineer(name, projs, mach) {
+    this.base = WorkerBee;
+    this.base(name, 'engineering', projs)
+    this.machine = mach || '';
+}
+// 新しい Engineer オブジェクトを作成する
+var jane = new Engineer('Doe, jane', ['navigator', 'javascript', 'belau']);
+
+// call() / apply() メソッドを使う
+function Engineer(name, projs, mach) {
+    WorkerBee.call(this, name, 'engineering', projs);
+    this.machine = mach || '';
+}
+
+
+// プロパティの継承、再び
+// ローカル値と値継承
+/* オブジェクトのプロパティにデフォルトの値を持たせて、実行時にデフォルト値を変更したいのであれば、コンストラクター関数内でなく、コンストラクターのプロトタイプ内でプロパティを設定するようにしてください。 */
+
+// インスタンス関係の決定
